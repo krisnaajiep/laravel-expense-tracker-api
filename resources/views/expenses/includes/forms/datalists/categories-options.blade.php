d4e28780695e2071f19e9f87ba0df18ca8cf785f
Krisna Ajie <krisnaajiep@gmail.com>
2024-10-04 05:58:29 +0700

    Remove some folders and files from tracking and add to .gitignore

--- resources/views/expenses/includes/forms/datalists/categories-options.blade.php
+++
@@ -1,8 +0,0 @@
-<datalist id="categoriesOptions">
-    <option value="Groceries">
-    <option value="Leisure">
-    <option value="Electronics">
-    <option value="Utilities">
-    <option value="Clothing">
-    <option value="Health">
-</datalist>
--- resources/views/expenses/includes/forms/delete/expense.blade.php
+++
@@ -1,5 +0,0 @@
-<form action="{{ route('expenses.destroy', ['expense' => $expense->id]) }}" method="post" class="d-inline">
-    @method('DELETE')
-    @csrf
-    <button type="submit" class="btn btn-danger btn-sm" onclick="confirm('Are you sure?')">Delete</button>
-</form>
