9d0f248a0c8288cd69ee6c4f481bd31ded8ebff2
Krisna Ajie <krisnaajiep@gmail.com>
2024-10-04 21:35:49 +0700

    Delete laravel sanctum

--- routes/api/api.php
+++ routes/api/api.php
@@ -5,10 +5,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\ProfileController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
 Route::middleware(['api', 'auth:api', 'api-custom-verified', 'throttle:api'])->group(function () {
     Route::controller(ProfileController::class)->group(function () {
         Route::put('/profile', 'update');
