0b20bd47271f63dea7f53d9c8c14868c6b88de1e
Krisna Ajie <krisnaajiep@gmail.com>
2025-01-29 18:32:22 +0700

    feat(migration): add foreign key for user and additional fields to expenses table

--- database/migrations/2025_01_29_091212_create_expenses_table.php
+++ database/migrations/2025_01_29_091212_create_expenses_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use App\Models\User;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,6 +14,12 @@ return new class extends Migration
     {
         Schema::create('expenses', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(User::class)->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->decimal('amount', total: 10, places: 2);
+            $table->string('category', length: 50);
+            $table->text('description')->nullable();
             $table->timestamps();
         });
     }
